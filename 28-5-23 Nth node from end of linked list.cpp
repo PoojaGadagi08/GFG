bf8d5f654ef92dec2ff186ef5b69d1078312ab01
skya3 <107419125+PoojaGadagi08@users.noreply.github.com>
2023-05-28 08:30:38 +0530

    Create 28-5-23 Nth node from end of linked list.cpp

---
+++ 28-5-23 Nth node from end of linked list.cpp
@@ -0,0 +1,38 @@
+Given a linked list consisting of L nodes and given a number N. The task is to find the Nth node from the end of the linked list.
+
+Example 1:
+
+Input:
+N = 2
+LinkedList: 1->2->3->4->5->6->7->8->9
+Output: 8
+Explanation: In the first example, there
+are 9 nodes in linked list and we need
+to find 2nd node from end. 2nd node
+from end is 8.  
+  
+  
+  
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to find the data of nth node from the end of a linked list.
+int getNthFromLast(Node *head, int n)
+{
+       Node* slow=head,*fast=head;
+       while(n-- && fast)fast=fast->next;
+           
+      if(n>=0 && !fast)return -1;
+       while(fast){
+           slow=slow->next;
+           fast=fast->next;
+       }
+       return slow->data;
+}
