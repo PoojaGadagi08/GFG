beae8dad29f277dda59b843c71e4798edc9e5999
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-07-21 12:44:09 +0530

    Create 21-7-23 Reverse a Linked List in groups of given size.java

---
+++ 21-7-23 Reverse a Linked List in groups of given size.java
@@ -0,0 +1,90 @@
+Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should be considered as a group and must be reversed (See Example 2 for clarification).
+
+Example 1:
+
+Input:
+LinkedList: 1->2->2->4->5->6->7->8
+K = 4
+Output: 4 2 2 1 8 7 6 5 
+Explanation: 
+The first 4 elements 1,2,2,4 are reversed first 
+and then the next 4 elements 5,6,7,8. Hence, the 
+resultant linked list is 4->2->2->1->8->7->6->5.
+Example 2:
+
+Input:
+LinkedList: 1->2->3->4->5
+K = 3
+Output: 3 2 1 5 4 
+Explanation: 
+The first 3 elements are 1,2,3 are reversed 
+first and then elements 4,5 are reversed.Hence, 
+the resultant linked list is 3->2->1->5->4.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function reverse() which should reverse the linked list in group of size k and return the head of the modified linked list.
+
+Expected Time Complexity : O(N)
+Expected Auxilliary Space : O(1)
+
+
+
+
+  
+/*node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution
+{
+   public static Node reverse(Node head, int k)
+    {
+        // Base case: if the linked list is empty or k is 0 or 1
+        if (head == null || k <= 1) {
+            return head;
+        }
+
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        int count = 0;
+
+        // Reverse the first k nodes of the linked list
+        while (count < k && current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+            count++;
+        }
+
+        // Recursively reverse the next k nodes and connect them to the current group
+        if (next != null) {
+            head.next = reverse(next, k);
+        }
+
+        // prev is the new head of the reversed group
+        return prev;
+    }
+
+    // Helper function to print the linked list
+    public static void printLinkedList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
