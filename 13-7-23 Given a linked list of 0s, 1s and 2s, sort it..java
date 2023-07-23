c3e198f26dd96389baf6242e9735a3a0d33fd7cb
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-07-23 11:30:35 +0530

    Create 13-7-23 Given a linked list of 0s, 1s and 2s, sort it..java

---
+++ 13-7-23 Given a linked list of 0s, 1s and 2s, sort it..java
@@ -0,0 +1,76 @@
+Given a linked list of N nodes where nodes can contain values 0s, 1s, and 2s only. The task is to segregate 0s, 1s, and 2s linked list such that all zeros segregate to head side, 2s at the end of the linked list, and 1s in the mid of 0s and 2s.
+
+Example 1:
+
+Input:
+N = 8
+value[] = {1,2,2,1,2,0,2,2}
+Output: 0 1 1 2 2 2 2 2
+Explanation: All the 0s are segregated
+to the left end of the linked list,
+2s to the right end of the list, and
+1s in between.
+Example 2:
+
+Input:
+N = 4
+value[] = {2,2,0,1}
+Output: 0 1 2 2
+Explanation: After arranging all the
+0s,1s and 2s in the given format,
+the output will be 0 1 2 2.
+Your Task:
+The task is to complete the function segregate() which segregates the nodes in the linked list as asked in the problem statement and returns the head of the modified linked list. The printing is done automatically by the driver code.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 <= N <= 106
+
+
+  
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution
+{
+    //Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head)
+    {
+        int cnt[]=  new int[3];
+        for(Node curr=head; curr!=null; curr=curr.next)
+        {
+            if(curr.data==0) cnt[0]++;
+            else if(curr.data==1) cnt[1]++;
+            else cnt[2]++;
+        }
+        Node curr=head;
+        int idx=0;
+        while(curr!=null)
+        {
+            if(cnt[idx]==0)
+            idx++;
+            
+            if(cnt[idx]!=0)
+            {
+                curr.data = idx;
+                cnt[idx]--;
+                curr=curr.next;
+            }
+
+        }
+        return head;
+    }
+}
