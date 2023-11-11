506d5f82b68c95fa3d95c51b34b9b39a4b1addba
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-11-11 09:39:06 +0530

    Create 11-11-23 Isomorphic Strings.java

---
+++ 11-11-23 Isomorphic Strings.java
@@ -0,0 +1,74 @@
+Given two strings 'str1' and 'str2', check if these two strings are isomorphic to each other.
+
+If the characters in str1 can be changed to get str2, then two strings, str1 and str2, are isomorphic. A character must be completely swapped out for another character while maintaining the order of the characters. A character may map to itself, but no two characters may map to the same character.
+
+Example 1:
+
+Input:
+str1 = aab
+str2 = xxy
+Output: 
+1
+Explanation: 
+There are two different characters in aab and xxy, i.e a and b with frequency 2 and 1 respectively.
+Example 2:
+
+Input:
+str1 = aab
+str2 = xyz
+Output: 
+0
+Explanation: 
+There are two different characters in aab but there are three different charactersin xyz. So there won't be one to one mapping between str1 and str2.
+Your Task:
+You don't need to read input or print anything.Your task is to complete the function areIsomorphic() which takes the string str1 and string str2 as input parameter and  check if two strings are isomorphic. The function returns true if strings are isomorphic else it returns false.
+
+Expected Time Complexity: O(|str1|+|str2|).
+Expected Auxiliary Space: O(Number of different characters).
+Note: |s| represents the length of string s.
+
+Constraints:
+1 <= |str1|, |str2| <= 105
+
+
+
+
+  class Solution
+{
+    //Function to check if two strings are isomorphic.
+    public static boolean areIsomorphic(String str1,String str2)
+    {
+        // Your code here
+        
+        if(str1.length()!=str2.length())
+        {
+            return false;
+        }
+        HashMap<Character,Character> map = new HashMap<>();
+        for(int i = 0; i < str1.length(); i++)
+        {
+            char original = str1.charAt(i);
+            char replacement = str2.charAt(i);
+            
+            if(!map.containsKey(original))
+            {
+                if(!map.containsValue(replacement))
+                {
+                    map.put(original,replacement);
+                }
+            else{
+                return false;
+            }
+            }else
+            {
+                char mapped = map.get(original);
+                if(mapped!=replacement){
+                    return false;
+                }
+            }
+            
+        }       
+        return true;
+        }
+
+}
