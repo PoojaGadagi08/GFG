c8bc3f7742058c32ee8ca916b40955121d523de9
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-12-25 14:45:25 +0530

    Create Diameter of a Binary Tree.java

---
+++ Diameter of a Binary Tree.java
@@ -0,0 +1,49 @@
+The diameter of a tree (sometimes called the width) is the number of nodes on the longest path between two end nodes. The diagram below shows two trees each with diameter nine, the leaves that form the ends of the longest path are shaded (note that there is more than one path in each tree of length nine, but no path longer than nine nodes). 
+
+
+
+Example 1:
+
+Input:
+       1
+     /  \
+    2    3
+Output: 3
+Example 2:
+
+Input:
+         10
+        /   \
+      20    30
+    /   \ 
+   40   60
+Output: 4
+Your Task:
+You need to complete the function diameter() that takes root as parameter and returns the diameter.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 <= Number of nodes <= 10000
+1 <= Data of a node <= 1000
+
+ 
+
+public class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] diameter = new int[1];
+        height(root, diameter);
+        return diameter[0];        
+    }
+
+    private int height(TreeNode node, int[] diameter) {
+        if (node == null) {
+            return 0;
+        }
+        int lh = height(node.left, diameter);
+        int rh = height(node.right, diameter);
+        diameter[0] = Math.max(diameter[0], lh + rh);
+        return 1 + Math.max(lh, rh);
+    }
+}
