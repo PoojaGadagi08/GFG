dcc5b478485b5d49e85f4be09a64ed2e7cb17273
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-12-27 15:55:26 +0530

    Create Top View of Binary Tree.java

---
+++ Top View of Binary Tree.java
@@ -0,0 +1,88 @@
+Given below is a binary tree. The task is to print the top view of binary tree. Top view of a binary tree is the set of nodes visible when the tree is viewed from the top. For the given below tree
+
+       1
+    /     \
+   2       3
+  /  \    /   \
+4    5  6   7
+
+Top view will be: 4 2 1 3 7
+Note: Return nodes from leftmost node to rightmost node. Also if 2 nodes are outside the shadow of the tree and are at same position then consider the left ones only(i.e. leftmost). 
+For ex - 1 2 3 N 4 5 N 6 N 7 N 8 N 9 N N N N N will give 8 2 1 3 as answer. Here 8 and 9 are on the same position but 9 will get shadowed.
+
+Example 1:
+
+Input:
+      1
+   /    \
+  2      3
+Output: 2 1 3
+Example 2:
+
+Input:
+       10
+    /      \
+  20        30
+ /   \    /    \
+40   60  90    100
+Output: 40 20 10 30 100
+Your Task:
+Since this is a function problem. You don't have to take input. Just complete the function topView() that takes root node as parameter and returns a list of nodes visible from the top view from left to right.
+
+Expected Time Complexity: O(NlogN)
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 ≤ N ≤ 105
+1 ≤ Node Data ≤ 105
+
+
+
+
+
+  
+class Pair {
+    Node node;
+    int hd;
+
+    public Pair(Node node, int hd) {
+        this.node = node;
+        this.hd = hd;
+    }
+}
+
+
+
+class Solution
+{
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    static ArrayList<Integer> topView(Node root)
+    {
+        ArrayList<Integer> ans = new ArrayList<>(); 
+        if(root == null) return ans;
+        Map<Integer, Integer> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<Pair>();
+        q.add(new Pair(root, 0)); 
+        while(!q.isEmpty()) {
+            Pair it = q.remove();
+            int hd = it.hd; 
+            Node temp = it.node; 
+            if(map.get(hd) == null) map.put(hd, temp.data); 
+            if(temp.left != null) {
+                
+                q.add(new Pair(temp.left, hd - 1)); 
+            }
+            if(temp.right != null) {
+                
+                q.add(new Pair(temp.right, hd + 1)); 
+            }
+        }
+    
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
+            ans.add(entry.getValue()); 
+        }
+        return ans; 
+        
+    }
+}
