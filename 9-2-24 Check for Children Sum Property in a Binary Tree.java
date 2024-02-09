1d82149487574ded23009b4f1936e0131a7af6f1
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2024-02-09 07:03:02 +0530

    Create 9-2-24 Check for Children Sum Property in a Binary Tree.java

---
+++ 9-2-24 Check for Children Sum Property in a Binary Tree.java
@@ -0,0 +1,91 @@
+Given a binary tree having n nodes. Check whether all of its nodes have the value equal to the sum of their child nodes. Return 1 if all the nodes in the tree satisfy the given properties, else it return 0.
+
+For every node, data value must be equal to the sum of data values in left and right children. Consider data value as 0 for NULL child.  Also, leaves are considered to follow the property.
+
+Example 1:
+
+Input:
+Binary tree
+       35
+      /   \
+     20  15
+    /  \  /  \
+   15 5 10 5
+Output: 
+1
+Explanation: 
+Here, every node is sum of its left and right child.
+Example 2:
+
+Input:
+Binary tree
+       1
+     /   \
+    4    3
+   /  
+  5    
+Output: 
+0
+Explanation: 
+Here, 1 is the root node and 4, 3 are its child nodes. 4 + 3 = 7 which is not equal to the value of root node. Hence, this tree does not satisfy the given condition.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function isSumProperty() that takes the root Node of the binary tree as input and returns 1 if all the nodes in the tree satisfy the following properties, else it returns 0.
+
+Expected Time Complexiy: O(n).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 <= n <= 105
+1 <= Data on nodes <= 105
+
+
+
+  
+
+//User function Template for Java
+
+
+/*Complete the function below
+Node is as follows:
+class Node{
+	int data;
+	Node left,right;
+	
+	Node(int key)
+	{
+	    data = key;
+	    left = right = null;
+	}
+}
+
+*/
+class Solution
+{
+    //Function to check whether all nodes of a tree have the value 
+    //equal to the sum of their child nodes.
+    public static int isSumProperty(Node root)
+    {
+        // add your code here
+        int [] a=new int [1];
+      a[0]=1;
+       func1(root,a);
+       
+      return a[0]; 
+    }
+    public static int func1(Node root,int [] a){
+      if(root == null)
+      return 0;
+      if(root.left==null && root.right==null)
+      return root.data;   
+      
+      int val1=func1(root.left,a);
+      int val2=func1(root.right,a);
+        
+        if(root.data!=(val1+val2)){
+        a[0]=0;}
+        
+        return root.data;
+    }
+}
+
+    
