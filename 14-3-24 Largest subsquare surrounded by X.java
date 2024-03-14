39fb3660c372781dadae8b5fa48ec66bb0435513
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2024-03-14 11:07:15 +0530

    Create 14-3-24 Largest subsquare surrounded by X.java

---
+++ 14-3-24 Largest subsquare surrounded by X.java
@@ -0,0 +1,103 @@
+Given a square matrix a of size n x n, where each cell can be either 'X' or 'O', you need to find the size of the largest square subgrid that is completely surrounded by 'X'. More formally you need to find the largest square within the grid where all its border cells are 'X'.
+
+Example 1:
+
+Input:
+n = 2
+a = [[X,X],
+     [X,X]]
+Output:
+2
+Explanation:
+The largest square submatrix 
+surrounded by X is the whole 
+input matrix.
+Example 2:
+
+Input:
+n = 4
+a = [[X,X,X,O],
+     [X,O,X,X],
+     [X,X,X,O],
+     [X,O,X,X]]
+Output:
+3
+Explanation:
+Here,the input represents following 
+matrix of size 4 x 4
+X X X O
+X O X X
+X X X O
+X O X X
+The square submatrix starting at 
+(0,0) and ending at (2,2) is the 
+largest submatrix surrounded by X.
+Therefore, size of that matrix would be 3.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function largestSubsquare() which takes the integer n and the matrix a as input parameters and returns the size of the largest subsquare surrounded by 'X'.
+Expected Time Complexity: O(n2)
+Expected Auxillary Space: O(n2)
+
+Constraints:
+1 <= n <= 1000
+a[i][j] belongs to {'X','O'} 
+
+
+
+class Solution {
+    int largestSubsquare(int n, char a[][]) {
+        // code here
+          int[][] top = new int[n][n];
+        int[][] left = new int[n][n];
+
+        // Top metric
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i][j] == 'X') {
+                    if (i != 0)
+                        top[i][j] = top[i - 1][j] + 1;
+                    else
+                        top[i][j] = 1;
+                }
+            }
+        }
+
+        // Left metric
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i][j] == 'X') {
+                    if (j != 0)
+                        left[i][j] = left[i][j - 1] + 1;
+                    else
+                        left[i][j] = 1;
+                }
+            }
+        }
+
+        int maxSubSq = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (top[i][j] == 0 || left[i][j] == 0)
+                    continue;
+
+                int currentValue = Math.min(top[i][j], left[i][j]);
+
+                while (currentValue > 0) {
+                    int top1 = i - currentValue + 1;
+                    int left1 = j - currentValue + 1;
+
+                    if ((left[top1][j] >= currentValue) && (top[i][left1] >= currentValue)) {
+                        maxSubSq = Math.max(maxSubSq, currentValue);
+                        break;
+                    }
+
+                    currentValue--;
+                }
+            }
+        }
+
+        return maxSubSq;
+        
+    }
+};
