d55cd2749f513c1c28034663eb1e87fe30d96c3b
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2024-05-12 11:31:14 +0530

    Create 12-5-24 Minimum steps to destination.java

---
+++ 12-5-24 Minimum steps to destination.java
@@ -0,0 +1,11 @@
+class Solution {
+    static int minSteps(int d) {
+        int cnt = 0;
+        int i=0;
+        while(i<d || (i-d)%2!=0){
+            cnt++;
+            i+=cnt;
+        }
+        return cnt;
+    }
+}
